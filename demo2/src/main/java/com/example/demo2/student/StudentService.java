fa69c85b12d0ce7dcf21fe680426a7b8579bb85c
bgoggins <74381778+bgoggins@users.noreply.github.com>
2021-10-19 18:27:49 -0400

    Add files via upload

---
+++ demo2/src/main/java/com/example/demo2/student/StudentService.java
@@ -0,0 +1,22 @@
+package com.example.demo2.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "mari","mari.jamal@gmail.com",
+                        21,
+                        LocalDate.of(2000, Month.AUGUST,5)
+                )
+        );
+    }
+}
